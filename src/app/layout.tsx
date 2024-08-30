6c73262409016e576669cc8e225d259701799a43
Sungmin Lee <sungminlee417@gmail.com>
2024-08-30 10:51:29 -0700

    add script for identity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,8 +18,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <script
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
+          async
+        ></script>
         <MainNav />
         <main>{children}</main>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", (user) => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+            `,
+          }}
+        />
       </body>
     </html>
   );
