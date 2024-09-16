dd76c3ffa21b3e5be765aac85750fbb7ee0bbf16
Sungmin Lee <sungminlee417@gmail.com>
2024-09-16 10:24:40 -0700

    edit image config

--- content/gallery.md
+++ content/gallery.md
@@ -1,5 +1,5 @@
 ---
-photos:
+images:
   - image: img/gallery-pic1.png
     alt: luke playing the piano
 ---
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,12 +2,7 @@
 
 import React from "react";
 import Masonry from "react-masonry-css";
-import image1 from "../../public/images/gallery/gallery-pic1.png";
-import image2 from "../../public/images/gallery/gallery-pic10.png";
-import image3 from "../../public/images/gallery/gallery-pic11.png";
-import image4 from "../../public/images/gallery/gallery-pic12.png";
-import image5 from "../../public/images/gallery/gallery-pic2.png";
-import image6 from "../../public/images/gallery/gallery-pic3.png";
+import { attributes } from "../../content/gallery.md";
 
 interface Image {
   src: string;
@@ -15,21 +10,14 @@ interface Image {
 }
 
 const Gallery = () => {
+  const { images } = attributes as { images: Image[] };
+
   const breakpointColumnsObj = {
     default: 4,
     1100: 3,
     700: 2,
     500: 1,
   };
-
-  const images: Image[] = [
-    { src: image1.src, alt: "image1" },
-    { src: image2.src, alt: "image2" },
-    { src: image3.src, alt: "image3" },
-    { src: image4.src, alt: "image4" },
-    { src: image5.src, alt: "image5" },
-    { src: image6.src, alt: "image6" },
-  ];
 
   return (
     <section className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
