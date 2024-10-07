d4fa3b892c4ab4c6ec42e64fffd8917342dd9741
Sungmin Lee <sungminlee417@gmail.com>
2024-10-07 12:43:48 -0700

    remove favicon from head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,9 +23,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <body className={inter.className}>
         <script
           src="https://identity.netlify.com/v1/netlify-identity-widget.js"
