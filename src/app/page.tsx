43b55318b3d9b31d9878e2106c8fe3d2a9ba550b
Sungmin Lee <sungminlee417@gmail.com>
2024-10-07 12:44:58 -0700

    remove album section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/components/Hero";
 import Biography from "@/components/Biography";
-import Album from "@/components/Album";
+// import Album from "@/components/Album";
 import UpcomingConcerts from "@/components/UpcomingConcerts";
 import ContactForm from "@/components/ContactForm";
 import Gallery from "@/components/Gallery";
