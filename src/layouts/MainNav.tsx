345f938256617e722c7b0df908cfb864025dd13d
Sungmin Lee <sungminlee417@gmail.com>
2025-08-09 11:39:45 -0700

    fix: make hamburger menu more visible on mobile

--- src/layouts/MainNav.tsx
+++ src/layouts/MainNav.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 text-neutral"
+                className="h-6 w-6 text-neutral dark:text-gray-200"
                 fill="none"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
